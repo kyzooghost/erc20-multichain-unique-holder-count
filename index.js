ac5f14b7a5cf90588214e2460f00706028c762dd
kyzooghost <jeffersonzjtang@gmail.com>
2022-05-21 00:54:55 +1000

    fixed bug with polygon rpc

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ main()
 async function main() {
     const [mainnetHoldersMap, polygonHoldersMap, auroraHoldersMap] = await Promise.all([
         getHoldersMapping(mainnetContract),
-        getHoldersMapping(polygonContract),
+        getPolygonHoldersMapping(polygonContract),
         getHoldersMapping(auroraContract)
     ])
 
@@ -110,4 +110,72 @@ function getUniqueHoldersList(...mapArgs) {
     }
 
     return holdersSet
+}
+
+async function getPolygonHoldersMapping(contract) {
+    const holders_map = new Map()
+
+    // Get all Transfer events
+    let filter = contract.filters.Transfer()
+    
+    const latestBlock = await contract.provider.getBlockNumber();
+
+    // Needed to break up Polygon log query into two - was getting "Log response size exceeded" error
+    const [events_0, events_1] = await Promise.all([
+        contract.queryFilter(filter, 0, 28453267),
+        contract.queryFilter(filter, 28453268, latestBlock)
+    ])
+
+    const events = [...events_0, ...events_1]
+
+    // need to sort by blockNumber as first precedence
+    // then sort by transactionIndex
+    events.sort(function (a, b) {
+        if (a.blockNumber == b.blockNumber) {
+            return (a.transactionIndex - b.transactionIndex)
+        } else {
+            return (a.blockNumber - b.blockNumber)
+        }
+    })
+
+    // Loop through every event
+    for (const event of events) {
+        const { from, to, value } = event.args
+
+        try {
+            // Exclude 0 value transactions
+            if (value.eq(ZERO)) {
+                continue;
+            }
+
+            // Subtract value from 'from', except if from == zero address
+            if (from != ZERO_ADDRESS) {
+                // If "from" key doesn't exist, create new map entry with value = ZERO
+                const current_from_holding = holders_map.get(from) ? holders_map.get(from) : ZERO
+                holders_map.set(from, current_from_holding.sub(value))
+            }
+
+            // Add value to 'to'
+            const current_to_holding = holders_map.get(to) ? holders_map.get(to) : ZERO
+            holders_map.set(to, current_to_holding.add(value))
+
+        } catch(e) {
+            // Catch block for troubleshooting and finding troublesome transactions
+            console.log(event)
+            console.log("------")
+            console.error(e)
+        }
+    }
+
+    // Iterate through mapping and remove zero balance addresses
+    for (let [key, value] of holders_map) {
+        if (value.eq(ZERO)) {
+            holders_map.delete(key)
+        }
+    }
+
+    // Delete zero address
+    holders_map.delete(ZERO_ADDRESS)
+
+    return holders_map
 }
